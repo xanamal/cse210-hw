2cbb217c1baa808a6ac814b09db3eef9d22b70d3
xanamal <xanenthegreat@gmail.com>
2026-05-06 22:00:21 -0700

    added guess counter with exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int guessCount = 0;
+        while (guess != randomNumber)
+        {
+            Console.Write("Guess a number between 1 and 100: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess))
+            {
+                guessCount++;
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the number!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+        Console.WriteLine($"It took you {guessCount} guesses to find the number.");
+
+        Console.Write("Do you want to play again? (yes/no): ");
+        string playAgain = Console.ReadLine().ToLower();
+
+        if (playAgain == "yes")
+        {
+            Main(args);
+        }
+        else
+        {
+            Console.WriteLine("Thanks for playing! Goodbye!");
+        }
     }
 }
